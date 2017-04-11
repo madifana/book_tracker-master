ce5584dde3ac632ed4425781c7bb97e4c0cd1aa6
madifana <madifana@yahoo.com>
2017-04-11 14:11:39 -0700

    defined signup post controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
 			erb :'users/create_user'
 		end
 	end
+
+	post '/signup' do 
+		if params["username"] == "", || params[:password] == "" || params[:email] ""
+			redirect '/signup'
+		else
+			@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+			@user.save
+			session[:user_id] = @user.id 
+			redirect '/books'
+		end
+	end
 end
