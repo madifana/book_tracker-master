82c6ac8a7be37d9e2e131ac0f277bd00192dbbf6
madifana <madifana@yahoo.com>
2017-04-11 13:22:43 -0700

    created slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
 	def slug
 		username.downcase.strip.gsub(" ","-")
 	end
+
+	def self.find_by_slug(slug)
+		User.all.find do |u|
+			u.slug == slug
+		end
+	end
 end
