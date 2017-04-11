5f3a2e6bc5c8b8cf94068705ed84f4e1415c32bc
madifana <madifana@yahoo.com>
2017-04-11 14:16:44 -0700

    created login get controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,21 @@ class UsersController < ApplicationController
 	end
 
 	post '/signup' do 
-		if params["username"] == "", || params[:password] == "" || params[:email] ""
+		if params["username"] == "" || params[:password] == "" || params[:email] ""
 			redirect '/signup'
 		else
 			@user = User.new(username: params[:username], email: params[:email], password: params[:password])
 			@user.save
 			session[:user_id] = @user.id 
 			redirect '/books'
+		end
+	end
+
+	get '/login' do 
+		if logged_in?
+			redirect '/books'
+		else
+			erb :'/users/login'
 		end
 	end
 end
