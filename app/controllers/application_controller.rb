0a0cc9ae35c34901958210f1d23b468e77247a8e
madifana <madifana@yahoo.com>
2017-04-11 13:42:08 -0700

    created index controller and session helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,18 @@ class ApplicationController < Sinatra::Base
 		enable :sessions
 		set :session_secret, "secret"
 	end
+
+	get '/' do 
+		erb :index
+	end
+
+	helpers do 
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
 end
