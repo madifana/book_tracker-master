011bb83a386ced5db3f7a099869232ad9f66d542
madifana <madifana@yahoo.com>
2017-04-12 15:09:06 -0700

    corrected author in books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412213738) do
+ActiveRecord::Schema.define(version: 20170412220736) do
 
   create_table "books", force: :cascade do |t|
     t.string  "title"
-    t.string  "Author"
+    t.string  "author"
     t.integer "user_id"
   end
 
