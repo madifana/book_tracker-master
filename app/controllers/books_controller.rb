4f878de2f32f2d9349fd7a849dfea67aca6590a9
madifana <madifana@yahoo.com>
2017-04-12 13:27:54 -0700

    added the books post controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,22 @@
 class BooksController < ApplicationController
+	
+	get '/books/new' do
+		if !logged_in?
+			redirect '/login'
+		else
+			erb :new
+		end
+    end
+
+    post '/books' do
+		if !logged_in?
+			redirect '/login'
+		else
+			@post = Post.create(params)
+			erb :index
+		end
+	end
+
 	get '/books' do 
 		if !logged_in?
 			redirect '/login'
@@ -7,12 +25,4 @@ class BooksController < ApplicationController
 			erb :'/books/books'
 		end
 	end
-
-	get '/books/new' do
-		if !logged_in?
-			redirect '/login'
-		else
-			erb :new
-		end
-    end
 end
