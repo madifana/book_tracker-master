8014e5d6226da5042c484fe323485707932ade26
madifana <madifana@yahoo.com>
2017-04-12 16:30:22 -0700

    changed /signup redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
 	get '/signup' do 
 		if logged_in?
-			redirect '/books'
+			redirect '/users/#{create_user.slug}'
 		else
 			erb :'users/create_user'
 		end
