2801d268bd110168adbf9ceb29b56a07d48a76d7
madifana <madifana@yahoo.com>
2017-04-13 15:45:36 -0700

    fixed flash messages

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ActiveRecord::Base
 	belongs_to :user
+	validates_presence_of :title, :author, :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :books
 	has_secure_password
+	validates_presence_of :username
+	validates :email, :username, uniqueness: true
+	validate :is_email_available?, :is_username_available?
 
 	def slug
 		username.downcase.strip.gsub(" ","-")
@@ -11,4 +14,14 @@ class User < ActiveRecord::Base
 			u.slug == slug
 		end
 	end
+
+	protected
+
+    def is_email_available?
+      errors.add(:email, "The email #{email} has already been taken.") if User.find_by(email: email)
+    end
+
+    def is_username_available?
+        errors.add(:username, "The username #{username} has already been taken.") if User.find_by(username: username)
+    end
 end
